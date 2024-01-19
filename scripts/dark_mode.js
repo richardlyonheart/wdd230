4c9caed44db4bbd92e90594fa4db936fd5d7eed7
richardlyonheart <brendenlyon907@gmail.com>
2024-01-19 13:20:40 -0600

    dark mode

---
+++ scripts/dark_mode.js
@@ -0,0 +1,17 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const body = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+        body.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+        body.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
