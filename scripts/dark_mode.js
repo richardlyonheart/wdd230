d2943db733fb89fc1721f7390761c112ce243426
richardlyonheart <brendenlyon907@gmail.com>
2024-01-19 13:33:52 -0600

    darkmode enhancements

--- scripts/dark_mode.js
+++ scripts/dark_mode.js
@@ -1,17 +1,19 @@
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
 const body = document.querySelector("body");
+const header = document.querySelector("header");
+const nav = document.querySelector("nav");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🕶️")) {
 		main.style.background = "#000";
 		main.style.color = "#fff";
-        body.style.color = "#fff";
+        body.style.backgroundColor = "#000";
 		modeButton.textContent = "🔆";
 	} else {
-		main.style.background = "#eee";
+		main.style.background = "#fff";
 		main.style.color = "#000";
-        body.style.color = "#000";
+        body.style.backgroundColor = "#fff";
 		modeButton.textContent = "🕶️";
 	}
 });
