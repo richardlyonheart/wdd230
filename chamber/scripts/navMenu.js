4b2aeffba821a609ab00e1366d2a856594397557
richardlyonheart <brendenlyon907@gmail.com>
2024-01-21 00:05:45 -0600

    nav menu fixed mobile

--- chamber/scripts/navMenu.js
+++ chamber/scripts/navMenu.js
@@ -1,12 +1,10 @@
-const burgerButton = document.querySelector('#menu')
-var menu = document.querySelector('.navlist')
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navlist');
 
-burgerButton.addEventListener('click', () => {
-    if (burgerButton.textContent.includes ("☰") && window.innerWidth < 1000) {
-        menu.style.display = 'grid';
-        burgerButton.textContent = "X";
-    } else {
-        burgerButton.textContent = '☰';
-        menu.style.display = 'none';
-    }
-})
+
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+  
+hamButton.classList.toggle('open');
+});
