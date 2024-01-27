6fd6f031a7f8135c0ecaef1fb44bf101ec6e199b
richardlyonheart <brendenlyon907@gmail.com>
2024-01-27 13:33:13 -0600

    finished map and resizing of hero images

---
+++ week03/scripts/bom.js
@@ -0,0 +1,36 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+let chaptersArray = getChapterList() || [];
+
+button.addEventListener('click', () => {
+    if (input.value != '') {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = '';
+        input.focus();
+    }   
+    else {
+        window.alert('Please enter in a favorite chapter.');
+    }
+});
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter)
+});
+function displayList(item) {
+    let li = document.createElement('li');
+    let deletebutton = document.createElement('button');
+    li.textContent = item; 
+    deletebutton.textContent = '❌';
+    deletebutton.classList.add('delete');
+    li.append(deletebutton);
+    list.append(li);
+    deletebutton.addEventListener('click', function () {
+      list.removeChild(li);
+      deleteChapter(li.textContent);
+      input.focus();
+    });
+}
