4b44596f6fc769e049bbaac76e2bf94da594a071
richardlyonheart <brendenlyon907@gmail.com>
2024-01-28 00:18:43 -0600

    visiting data

--- chamber/scripts/pageCounter.js
+++ chamber/scripts/pageCounter.js
@@ -1,13 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (!localStorage.lastVisit) {
+        localStorage.lastVisit = Date.now();
+        displayMessage("Welcome! Let us know if you have any questions.");
+    } else {
+    	var lastVisitTime = parseInt(localStorage.lastVisit);
+        var currentTime = Date.now();
+        var timeDifference = currentTime - lastVisitTime;
+        var daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        if (daysDifference < 1) {
+          displayMessage("Back so soon! Awesome!");
+        } else {
+          var daysText = (daysDifference === 1) ? "day" : "days";
+          displayMessage("You last visited " + daysDifference + " " + daysText + " ago.");
+        }
+        localStorage.lastVisit = currentTime;
+      }
 
-const visitsDisplay = document.querySelector(".visits");
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-
-if (numVisits != 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `First time? try the donuts.`;
-}
-
-numVisits++;
-
-localStorage.setItem("numVisits-ls", numVisits);
+function displayMessage(message) {
+    document.getElementById('visits').innerText = message;
+    }
+});
