82a644b5a6dab915e5ba55d347c2747bd6cc8fda
richardlyonheart <brendenlyon907@gmail.com>
2024-02-07 21:24:16 -0500

    small addition to json

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -1,10 +1,10 @@
 const url='https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json'; //define the source of information
 const cards = document.querySelector('#cards'); // where information to be displayed
 
-async function getProphetData() {
+async function getProphetData(url) {
     const response = await fetch(url); //fetch information
     const data = response.json; // store information
-    //console.table(data.prophets); // check data response in console
+    console.table(data.prophets); // check data response in console
     displayProphets(data.prophets)
 }
 
