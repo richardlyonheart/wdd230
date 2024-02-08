fc7125ceb5f48ff9d211c1592e967abf4f42b962
richardlyonheart <brendenlyon907@gmail.com>
2024-02-08 18:12:06 -0500

    without url

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -1,27 +1,29 @@
-const url='https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json'; //define the source of information
-const cards = document.querySelector('#cards'); // where information to be displayed
+const url="https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json"; //define the source of information
+const cards = document.querySelector("#cards"); // where information to be displayed
 
-async function getProphetData(url) {
+async function getProphetData() {
     const response = await fetch(url); //fetch information
     const data = response.json; // store information
     console.table(data.prophets); // check data response in console
-    displayProphets(data.prophets)
+    displayProphets(data.prophets);
 }
 
 function displayProphets(){
-    const displayProphets = (prophets) => {
+    const displayProphets() = (prophets) => {
+       
         prophets.forEach((prophet) => {
-            let card = document.createElement('section');
-            let fullName = document.createElement('h2');
-            let portrait = document.createElement('img');
-            fullName.textContent = '${prophet.name} {prophet.lastName}'; //pulls from the source and compiles
+            let card = document.createElement("section");
+            let fullName = document.createElement("h2");
+            let portrait = document.createElement("img");
+            fullName.textContent = '${prophet.name} ${prophet.lastName}'; //pulls from the source and compiles
             portrait.setAttribute('src', prophet.imageurl);//pulls url from source
-            portrait.setAttribute('alt', 'Potrait of ${prophet.name prophet.lastname}')
+            portrait.setAttribute('alt', 'Potrait of ${prophet.name} ${prophet.lastname}');
 
             card.appendChild(fullName);
             card.appendChild(portrait);
 
             cards.appendChild(card);
+
 
         });
     }
