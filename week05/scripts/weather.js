323d71b94ef29c48b2bf603d75f03962974b0af6
richardlyonheart <brendenlyon907@gmail.com>
2024-02-09 17:27:39 -0500

    weather

---
+++ week05/scripts/weather.js
@@ -0,0 +1,36 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid=414566a386a08cff91c0b2692629a943&units=imperial';
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        //console.log(data);
+        displayResults(data); 
+      } else {
+        throw Error(await response.text());
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  apiFetch(url);
+  
+  function displayResults(data) {
+      currentTemp.innerHTML = `${data.main.temp.toFixed(0)}&deg;F`;
+    
+      const weatherEvents = data.weather.map(event => {
+        const iconsrc = `https://openweathermap.org/img/w/${event.icon}.png`;
+        const desc = event.description.replace(/\b\w/g, l => l.toUpperCase());
+        weatherIcon.setAttribute('src', iconsrc);
+        weatherIcon.setAttribute('alt', desc);
+    
+        return `<p>${desc}</p>`;
+      });
+    
+      captionDesc.innerHTML = weatherEvents.join('');
+    }
