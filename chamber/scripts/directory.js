83bba07aa0df77517c4b7fae0cb9a75ebfc22efb
richardlyonheart <brendenlyon907@gmail.com>
2024-02-11 11:14:52 -0500

    json fixes

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,32 +1,40 @@
-const url="https://richardlyonheart.github.io/wdd230/chamber/data/directory.json"; //define the source of information
-const cards = document.querySelector("#cards"); // where information to be displayed
+const url="https://richardlyonheart.github.io/wdd230/chamber/data/members.json"; //define the source of information
+const cards = document.querySelector("#members"); // where information to be displayed
 
-async function getProphetData() {
+async function getMembersData() {
     const response = await fetch(url); //fetch information
     const data = await response.json(); // store information
     // console.table(data.prophets); // check data response in console
-    displayProphets(data.prophets);
+    displayMembers(data.displayMembers);
 }
 
 
-const displayMembers = (member) => {
-    prophets.forEach((prophet) => {
+const displayMembers = (members) => {
+    members.forEach((member) => {
         let card = document.createElement("section");
         let fullName = document.createElement("h2");
+        let address = document.createElement("p");
+        let membershipLevel = document.createElement("p");
         let portrait = document.createElement("img");
 
-        fullName.textContent = `${prophet.name} ${prophet.lastname}`; //pulls from the source and compiles
-        
-        portrait.setAttribute('src', prophet.imageurl);//pulls url from source
-        portrait.setAttribute('alt', 'Potrait of ${prophet.name} ${prophet.lastname}');
+        fullName.textContent = 'NAME: '`${info.name}`; //pulls from the source and compiles
+        address.textContent = 'ADDRESS: '`${info.address}`;
+        membershipLevel.textContent = 'MEMBERSHIP: '`${info.level}`;
+
+        portrait.setAttribute('src', info.img);//pulls url from source
+        portrait.setAttribute('alt', 'image of: ' `${member}`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
 
 
         card.appendChild(fullName);
+        card.appendChild(address);
+        card.appendChild(membershipLevel);
         card.appendChild(portrait);
 
-        cards.appendChild(card);
+        member.appendChild(card);
         });
     }
+
+    getMembersData(url);
