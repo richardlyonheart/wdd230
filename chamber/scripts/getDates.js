13c425f70e9f856f4a13e77db50db664204ccdc4
richardlyonheart <brendenlyon907@gmail.com>
2024-02-19 10:49:36 -0500

    edits on images and logos

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -16,7 +16,7 @@ const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=29.30&lon=-94.80&appid=414566a386a08cff91c0b2692629a943&units=imperial';
-const weatherForcast = 'https://api.openweathermap.org/data/2.5/forecast/daily?lat=29.30&lon=94.80&cnt=3&appid=241fe8e53ef22c16a2d5eb0155bed395&units=imperial';
+const weatherForcast = 'https://api.openweathermap.org/data/2.5/forecast?lat=29.30&lon=94.80&cnt=3&appid=241fe8e53ef22c16a2d5eb0155bed395&units=imperial';
 async function apiFetch() {
     try {
       const response = await fetch(weatherUrl);
@@ -48,12 +48,28 @@ async function apiFetch() {
     
       captionDesc.innerHTML = weatherEvents.join('');
     }
-    apiFetch(weatherForcast);
 
-    function displayForcast(data) {
-      forcastTemp.innerHTML = `${list.temp.day.toFixed(0)}&deg;F`;
+    async function apiFetch2() {
+      try {
+        const response = await fetch(weatherForcast);
+        if (response.ok) {
+          const list = await response.json();
+          //console.log(data);
+          displayForcast(list); 
+        } else {
+          throw Error(await response.text());
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
     
-      const weatherEvents = data.weather.map(event => {
+    apiFetch2(weatherForcast);
+
+    function displayForcast(list) {
+      forcastTemp.innerHTML = `${list.main.temp.toFixed(0)}&deg;F`;
+    
+      const weatherEvents = list.weather.map(event => {
         const iconsrc = `https://openweathermap.org/img/w/${event.icon}.png`;
         const desc = event.description.replace(/\b\w/g, l => l.toUpperCase());
         weatherIcon.setAttribute('src', iconsrc);
